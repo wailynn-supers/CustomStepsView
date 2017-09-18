2d6f315d485ce6e51461c1c712ad4e4f7ec94695
Wai Lynn Zaw <wailynnzaw@owaytrip.com>
2017-09-18 10:57:05 +0630

    Initial Commit

---
+++ CustomStepView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CustomStepView
+//
+//  Created by Wai Lynn Zaw on 9/18/17.
+//  Copyright © 2017 wailynnzaw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
